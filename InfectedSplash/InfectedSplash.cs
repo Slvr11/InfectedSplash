f1fe6fc6e16f0b746326a03e5d6d5cc286aa7d70
Slvr99 <bsawii@gmail.com>
2019-04-27 19:46:43 -0400

    Add project files.

---
+++ InfectedSplash/InfectedSplash.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InfinityScript;
+
+public class InfectedSplash : BaseScript
+{
+
+    public override void OnPlayerDisconnect(Entity player)
+    {
+        int teamPlayers = GSCFunctions.GetTeamPlayersAlive("axis");
+        if (player.SessionTeam == "axis" && teamPlayers == 0)
+        {
+            teamSplash("callout_deserted", player);
+        }
+    }
+    public static void teamSplash(string splash, Entity player)
+    {
+        foreach (Entity players in Players)
+        {
+            if (!players.IsPlayer) continue;
+            players.SetCardDisplaySlot(player, 5);
+            players.ShowHudSplash(splash, 1);
+        }
+    }
+}
